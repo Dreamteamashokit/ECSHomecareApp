b136bd493224912b3aec271669e9a7ac0da2532f
devanshdesai <desaidevansh89@gmail.com>
2022-05-23 09:56:52 +0530

    change for fix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   //domain : "https://localhost:44359",
+  
   domain : "https://api.eshcs.com",
    /* Invoice API URL */
    generateinvoiceurl : "/api/Invoice/generateinvoice",
