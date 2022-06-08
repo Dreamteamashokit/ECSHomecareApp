e3528da419370df28d81411f41cb3618737b6aae
Rajdeep-Das <rajdeepdas.india@gmail.com>
2022-06-08 11:25:43 +0530

    model update

--- src/app/models/client/client-billling-model.ts
+++ src/app/models/client/client-billling-model.ts
@@ -1,11 +1,42 @@
 import { BaseModel } from "../common";
 
 export class ClientBilling extends BaseModel {
-    billTo:Date;
-    fromDate:Date;
-    toDate: Date;
-    clientId:number;
-    authNumber:number;
-    serviceCode:string;
-    notes:string;
+    // billTo:Date;
+    // fromDate:Date;
+    // toDate: Date;
+    // clientId:number;
+    // authNumber:number;
+    // serviceCode:string;
+    // notes:string;
+
+  billingId: number;
+  payerId: number;
+  contractClientId: number;
+  authorizationNumber: string;
+  fromDate: string;
+  toDate: string;
+  hoursAuthorizedPerWeek: string;
+  hoursAuthorizedPerMonth: string;
+  hoursAuthorizedEntirePeriod: string;
+  serviceCode: number;
+  occurencesAuthorizedPerWeek: string;
+  occurencesAuthorizedPerMonth: string;
+  occurencesAuthorizedEntirePeriod: string;
+  daysOfWeekNotes: string;
+  brServiceCode_SAT: number;
+  brServiceCode_SUN: number;
+  brServiceCode_MON: number;
+  brServiceCode_TUE: number;
+  brServiceCode_WED: number;
+  brServiceCode_THU: number;
+  brServiceCode_FRI: number;
+  quantity_SAT: number;
+  quantity_SUN: number;
+  quantity_MON: number;
+  quantity_TUE: number;
+  quantity_WED: number;
+  quantity_THU: number;
+  quantity_FRI: number;
+  periodEpisode_Notes: string;
+  createdBy: number;
 }
