5f989ea1b233128ed2b78d96d5b712c0f8125568
Rajdeep-Das <rajdeepdas.india@gmail.com>
2022-06-10 00:51:43 +0530

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: true, 
-  domain : "https://localhost:5001",  
+  production: true,
+  domain: "https://localhost:44359",
   //domain : "https://api.eshcs.com",
-   /* Invoice API URL */
-   generateinvoiceurl : "/api/Invoice/generateinvoice",
-   getinvoicelisturl : "/api/Invoice/getinvoicelist",
-   getinvoicebyidurl : "/api/Invoice/getinvoicebyid",
-   payinvoicebyidurl : "/api/Invoice/payinvoice",
-   /* */
-  };
+  /* Invoice API URL */
+  generateinvoiceurl: "/api/Invoice/generateinvoice",
+  getinvoicelisturl: "/api/Invoice/getinvoicelist",
+  getinvoicebyidurl: "/api/Invoice/getinvoicebyid",
+  payinvoicebyidurl: "/api/Invoice/payinvoice",
+  /* */
+};
