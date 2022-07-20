e58034a8b65a0596ea794537685189c39e695d9e
rakesh.ardanuy <rakesh.gangwar@ardanuy.com>
2022-07-20 18:15:34 +0530

    update

---
+++ src/app/services/billing.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
+import { Observable } from 'rxjs';
+import { APIResponse } from '../models/api-response';
+import { ScheduleBillingModel, SearchSchedule } from 'src/app/models/billing/schedule-billing-model';
+@Injectable({
+  providedIn: 'root'
+})
+export class BillingService {
+
+  constructor(private _http: HttpClient) { }
+
+
+  GetAllScheduleBilling() {
+    debugger;
+    return this._http.get<APIResponse<ScheduleBillingModel[]>>(environment.domain + "/api/Billing/ScheduleBillingModel");
+  }
+
+
+  getAllScheduleBilling(search:SearchSchedule) {
+    debugger;
+    var headers_object = new HttpHeaders();
+    headers_object.append('Content-Type', 'application/json');
+    var headers_object = new HttpHeaders().set("Authorization", "Bearer " + "qatest");
+    const httpOptions = {
+      headers: headers_object
+    };
+    return this._http.post<APIResponse<ScheduleBillingModel[]>>(environment.domain + "/api/Billing/ScheduleBillingModel", search,httpOptions);       
+    
+  }
+
+
+
+
+
+
+
+
+
+
+}
